f9bbd95c2866b720b0926a377c9df08542428333
Kumar2396 <jtorres23@ucol.mx>
2022-05-19 10:30:14 -0500

    Add files via upload

---
+++ 6Integration/2do/tema6integración.py
@@ -0,0 +1,32 @@
+from math import sin, sqrt
+
+
+def fn(x):
+    return 2 * (sin(sqrt(x)) - x)
+
+a = 0
+b = 1.9724
+r1 = fn(a) * (b - a)
+
+print('Regla del Rectángulo I=', r1)
+
+
+m = (a + b) / 2
+r2 = fn(m) * (b - a)
+
+print('Regla del punto medio I=', r2)
+
+def fn1(y):
+    return (pow(7, -y)) + 3
+
+c = -1
+d = 2
+
+r3 = ((d-c) / 2) * (fn1(c) + fn1(d))
+
+print('Regla del Trapecio I=', r3)
+
+n = (c +d) / 2
+r4 = ((d - c) / 6) * (fn1(c) + 4*fn1(n) + fn1(d))
+
+print('Regla de Simpson I=', r4)
